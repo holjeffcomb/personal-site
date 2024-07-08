be1e0922d070043c46ba4b54e11feaf5bdbf073a
Jeff Holcomb <jeffh.84@gmail.com>
2024-07-07 18:05:20 -0700

    initial commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-between items-center w-screen border-solid border-b-2 border-black">
+      <h1 className="p-6 px-32 border-r-2 border-solid border-black">
+        Jeff Holcomb
+      </h1>
+      <p className="p-6 pr-32">links</p>
+    </div>
+  );
+}
